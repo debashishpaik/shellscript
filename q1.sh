f30a49611b66d29a727217d9bcd6ba19a3f62b4e
debashish paik <debashish.paik@niser.ac.in>
2019-08-10 00:01:03 +0530

    done

---
+++ q1.sh
@@ -0,0 +1,6 @@
+echo "plese enter ur name"
+read name
+mkdir $name
+touch $name/name$1.cpp
+echo "folder created"
+echo "file created"
---
+++ q2.sh
@@ -0,0 +1,10 @@
+echo "enter ur name
+read $name
+mkdir $name
+touch $name/name$1.cpp
+touch $name/name$2.cpp
+touch $name/name$3.cpp
+touch $name/name$4.cpp
+touch $name/name$5.cpp
+echo "all done
+"
