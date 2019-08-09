3497798b6330f13016256c60dcffc0c1cb512060
debashish paik <53595893+debashishpaik@users.noreply.github.com>
2019-08-10 00:03:03 +0530

    Update q2.sh

--- q2.sh
+++ q2.sh
@@ -1,4 +1,4 @@
-echo "enter ur name
+echo "enter ur name"
 read $name
 mkdir $name
 touch $name/name$1.cpp
@@ -6,5 +6,5 @@ touch $name/name$2.cpp
 touch $name/name$3.cpp
 touch $name/name$4.cpp
 touch $name/name$5.cpp
-echo "all done
-"
+echo "all done"
+
